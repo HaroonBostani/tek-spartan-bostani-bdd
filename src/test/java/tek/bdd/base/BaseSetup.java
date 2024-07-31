4d65ccb0e602979caf38fcb0dbd4560b036b1ec8
HaroonBostani <haroon7bostany@gmail.com>
2024-07-31 17:55:06 -0400

    setup class completed

---
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -0,0 +1,33 @@
+package tek.bdd.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseSetup {
+
+    // Encapsulating driver instance
+    private static WebDriver driver;
+
+    public void SetupBrowser () {
+       driver = new ChromeDriver();
+       driver.get("https://retail.tekschool-students.com/");
+       driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+   }
+
+   public void quizBrowser(){
+     //null check before quit
+       if (driver !=null) {
+           driver.quit();
+       }
+
+   }
+
+   //Giving read-only indirect access to driver.
+    // restrict setting new driver instance
+   public WebDriver getDriver() {
+        return driver;
+   }
+}
