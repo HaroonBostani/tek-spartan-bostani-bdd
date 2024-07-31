36eb035aae7f365093bab2068e4740686e586f2d
HaroonBostani <haroon7bostany@gmail.com>
2024-07-31 19:37:33 -0400

    Add hooks

---
+++ src/test/java/tek/bdd/steps/HooksSteps.java
@@ -0,0 +1,20 @@
+package tek.bdd.steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import tek.bdd.utility.SeleniumUtility;
+
+public class HooksSteps extends SeleniumUtility {
+
+    @Before
+    public void beforeEachScenario() {
+        SetupBrowser();
+    }
+
+    @After
+    public void afterEachScenario(){
+
+        quizBrowser();
+    }
+
+}
---
+++ src/test/resources/features/SetupTest.feature
@@ -0,0 +1,6 @@
+Feature: This feature to setup framework
+
+Scenario:  Validate top left corner logo
+ # Given Open browser and naviage to retial app
+  Then  Validate top letf corner is TEKSCHOOL
+  #Then Close the browser
