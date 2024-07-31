ba84979d8ede359ef8aaa2cdb2ccdda78c03baec
HaroonBostani <haroon7bostany@gmail.com>
2024-07-31 18:34:36 -0400

    make code maintainable

--- src/test/java/tek/bdd/utility/SeleniumUtility.java
+++ src/test/java/tek/bdd/utility/SeleniumUtility.java
@@ -2,6 +2,7 @@ package tek.bdd.utility;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import tek.bdd.base.BaseSetup;
@@ -14,6 +15,11 @@ public class SeleniumUtility extends BaseSetup {
         return new WebDriverWait(getDriver(),Duration.ofSeconds(20));
     }
 
+    private WebElement waitForVisibility(By locator){
+        return  getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+
     // Create a method to click on a given locator
     public void clickOnElement(By locator) {
         getWait().until(ExpectedConditions.elementToBeClickable(locator))
@@ -21,8 +27,12 @@ public class SeleniumUtility extends BaseSetup {
     }
 
     public void sendText(By locator, String value) {
-        getWait().until(ExpectedConditions.visibilityOfElementLocated(locator))
-                .sendKeys(value);
+       waitForVisibility(locator).sendKeys(value);
+    }
+
+    //Create method for getting the Text of a locator
+    public String getElementText(By locator) {
+        return waitForVisibility(locator).getText();
     }
 
 }
