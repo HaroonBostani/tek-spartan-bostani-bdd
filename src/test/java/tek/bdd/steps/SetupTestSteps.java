38dfdb9c9f9b8ccab3c9bfb3bf284b7019636600
HaroonBostani <haroon7bostany@gmail.com>
2024-08-01 10:45:37 -0400

    make code maintainable

---
+++ src/test/java/tek/bdd/steps/SetupTestSteps.java
@@ -0,0 +1,25 @@
+package tek.bdd.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+import tek.bdd.utility.SeleniumUtility;
+
+public class SetupTestSteps extends SeleniumUtility {
+
+    @Given("Given Open browser and naviage to retial app")
+  public void openBrowserAndNavigate() {
+       SetupBrowser();
+  }
+
+  @Then("Validate top letf corner is TEKSCHOOL")
+  public void validateTopLeftLogo() {
+  String text = getElementText(By.className("top-nav__logo active"));
+      System.out.println(text);
+  }
+
+  @Then("Close the browser")
+  public void closeBrowser() {
+        quizBrowser();
+  }
+}
